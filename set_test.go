7543f66b944ac51054a656f85c0b9c59fa6bf597
Yangfeng <smartwalle@gmail.com>
2017-07-02 18:54:07 +0800

    update

--- set_test.go
+++ set_test.go
@@ -1,1 +1,22 @@
 package dbr
+
+import "testing"
+
+func TestSession_SADD(t *testing.T) {
+	var s = getSession()
+	if _, err := s.SADD("set1", "1", "2", "3"); err != nil {
+		t.Fatal("SADD 指令错误", err)
+	}
+	s.Close()
+}
+
+func TestSession_SCARD(t *testing.T) {
+	var s = getSession()
+	if s.SCARD("set1") == 0 {
+		t.Fatal("SCARD 指令错误")
+	}
+	if s.SCARD("set2") != 0 {
+		t.Fatal("SCART 指令错误")
+	}
+	s.Close()
+}
