5e66e4d7a418b634b19de457077b9e43b2c1aebc
smartwalle <smartwalle@gmail.com>
2019-11-13 16:36:27 +0800

    添加 GEO

--- geo.go
+++ geo.go
@@ -35,6 +35,19 @@ func (this *Session) GEOPOS(key string, members ...string) *Result {
 	return this.Do("GEOPOS", ps...)
 }
 
+func (this *Session) GEORADIUS(key, longitude, latitude, radius string, unit GEOUnit, options ...string) *Result {
+	var ps = make([]interface{}, 0, 4+len(options))
+	ps = append(ps, key)
+	ps = append(ps, longitude)
+	ps = append(ps, latitude)
+	ps = append(ps, radius)
+	ps = append(ps, string(unit))
+	for _, opt := range options {
+		ps = append(ps, opt)
+	}
+	return this.Do("GEORADIUS", ps...)
+}
+
 func (this *Session) GEORADIUSBYMEMBER(key, member, radius string, unit GEOUnit, options ...string) *Result {
 	var ps = make([]interface{}, 0, 4+len(options))
 	ps = append(ps, key)
