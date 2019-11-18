c2933ece3a1bb1d99c6bb1cd8fc99029c16dfbc9
smartwalle <smartwalle@gmail.com>
2019-11-18 23:18:00 +0800

    更新注释

--- ext.go
+++ ext.go
@@ -14,15 +14,20 @@ func (this *Session) withBlock(key string, blockValue string, blockSeconds int64
 		return false, rResult
 	}
 
+	// 如果该 key 没有数据，则尝试对其进行执行写入操作
 	if rResult.Data == nil {
-		// 当从 redis 没有获取到数据的时候，写入 阻塞 数据
+		// 当从 redis 没有获取到数据的时候，写入 阻塞数据
 		if this.SET(key, blockValue, "EX", blockSeconds, "NX").MustString() == "OK" {
+			// 写入成功，直接返回不需要阻塞
 			return false, nil
 		}
+		// 写入失败，延迟再次调用
 		time.Sleep(time.Millisecond * 500)
 		return this.WithBlock(key, blockValue, blockSeconds)
 	}
 
+	// 如果该 key 有数据，并且数据为 阻塞数据，则尝试延迟再查询，防止首先写入 阻塞数据 的业务未完成
+	// 只能从一定程度上确保数据准确性
 	if rResult.MustString() == blockValue {
 		for i := 0; i < retryCount; i++ {
 			time.Sleep(retryDelay)
@@ -32,7 +37,7 @@ func (this *Session) withBlock(key string, blockValue string, blockSeconds int64
 			}
 		}
 
-		// 当从 redis 获取到数据，并且数据等于 阻塞 数据的时候，返回阻塞
+		// 该 key 有数据，并且数据等于 阻塞数据 的时候，返回阻塞
 		return true, rResult
 	}
 
