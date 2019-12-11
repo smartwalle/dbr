664ae6437b553d5d15560711aab68813c925e556
smartwalle <smartwalle@gmail.com>
2019-12-11 13:35:09 +0800

    task

--- sample/task/task.go
+++ sample/task/task.go
@@ -12,14 +12,14 @@ func main() {
 	var pool2 = dbr.NewRedis("192.168.1.77:6379", 10, 2, redis.DialDatabase(15))
 	var rs = dbr.NewRedSync(pool, pool2)
 
-	var key = "bb"
+	var key = "cc"
 
 	var tm = dbr.NewTaskManager(key, pool, dbr.WithTaskRedSync(rs))
 
-	tm.AddTask(fmt.Sprintf("%s1", key), "*/1 * * * *", func(name string) {
+	tm.AddTask(fmt.Sprintf("%s1", key), "*/3 * * * *", func(name string) {
 		fmt.Println(name, time.Now())
 	})
-	tm.AddTask(fmt.Sprintf("%s2", key), "*/1 * * * *", func(name string) {
+	tm.AddTask(fmt.Sprintf("%s2", key), "*/3 * * * *", func(name string) {
 		fmt.Println(name, time.Now())
 	})
 	select {}
