5bd00cfbf100b567aa1ac14c601e4e35f0a7bcfb
smartwalle <smartwalle@gmail.com>
2019-12-12 10:08:09 +0800

    将 TaskManager 重命名为 Cron

--- sample/cron/cron.go
+++ sample/cron/cron.go
@@ -13,14 +13,14 @@ func main() {
 	var pool3 = dbr.NewRedis("127.0.0.1:6381", 10, 2, redis.DialDatabase(15))
 	var rs = dbr.NewRedSync(pool, pool2, pool3)
 
-	var key = "cc"
+	var key = "aa"
 
-	var tm = dbr.NewTaskManager(key, pool, dbr.WithTaskRedSync(rs))
+	var c = dbr.NewCron(key, pool, dbr.WithCronRedSync(rs))
 
-	tm.AddTask(fmt.Sprintf("%s1", key), "*/1 * * * *", func(name string) {
+	c.Add(fmt.Sprintf("%s1", key), "*/1 * * * *", func(name string) {
 		fmt.Println(name, time.Now())
 	})
-	tm.AddTask(fmt.Sprintf("%s2", key), "*/1 * * * *", func(name string) {
+	c.Add(fmt.Sprintf("%s2", key), "*/1 * * * *", func(name string) {
 		fmt.Println(name, time.Now())
 	})
 	select {}
